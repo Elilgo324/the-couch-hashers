5e451879c2ee1b4e0202d98c2745a7e3b1d4d246
ItayElizur <elizur.itay@gmail.con>
2022-08-29 14:23:08 +0300

    finished parser

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ class Contributor:
         self.skillz = skillz_names
         self.skillz_levels = skillz_levels
 
+    def __repr__(self):
+        return f'Contributor(name={self.name}, skills={self.skillz})'
+
+
 class Project:
-    def __init__(self, project_name, project_length, project_score, project_deadline, roll_names, roll_levels):
+    def __init__(self, project_name: str, project_length: int, project_score: int, project_deadline: int,
+                 roll_names: List[str], roll_levels: List[int]):
         self.name = project_name
         self.length = project_length
         self.score = project_score
@@ -20,29 +25,38 @@ class Project:
         self.rolls = roll_names
         self.roll_levels = roll_levels
 
+    def __repr__(self):
+        return f'Project(name={self.name}, length={self.length}, score={self.score}, deadline={self.deadline},' \
+              f'rolls={self.rolls})'
 
 
 def parse():
-    num_contributors, num_projects = input()
+    num_contributors, num_projects = (int(x) for x in input().split(' '))
     contributors = []
     for contributor in range(num_contributors):
-        name, num_skillz = input()
+        contributor_intial = input().split(' ')
+        name = contributor_intial[0]
+        num_skillz = int(contributor_intial[1])
         skillz_names, skillz_levels = [], []
         for skill in range(num_skillz):
-            skill = input()
+            skill = input().split(' ')
             skillz_names.append(skill[0])
-            skillz_levels.append(skill[1])
+            skillz_levels.append(int(skill[1]))
         contributors.append(Contributor(name, skillz_names, skillz_levels))
 
     projects = []
     for project in range(num_projects):
-        project_name, project_length, project_score, project_deadline, project_rolls = input()
+        project_intial = input().split(' ')
+        project_name = project_intial[0]
+        project_length, project_score, project_deadline, project_rolls = (int(project_intial[x]) for x in range(1, 5))
         roll_names, roll_levels = [], []
         for roll in range(project_rolls):
-            roll = input()
+            roll = input().split(' ')
             roll_names.append(roll[0])
-            roll_levels.append(roll[1])
+            roll_levels.append(int(roll[1]))
         projects.append(Project(project_name, project_length, project_score, project_deadline, roll_names, roll_levels))
+
+    return contributors, projects
 
 
 def print_hi(name):
@@ -52,6 +66,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    contributors, projects = parse()
+    print(contributors)
+    print(projects)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
