d0dd614f7c0b0f3f8396999df050d55031498a71
ItayElizur <elizur.itay@gmail.con>
2022-08-29 16:02:56 +0300

    added environment

---
+++ master.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from environment import Contributor, Project
+from genetics import Gene
+
+
+class Environment:
+    def __init__(self, contributors: List[Contributor], projects: List[Project]):
+        self.contributors = contributors
+        self.projects = projects
+        self.max_days = max(project.latest for project in self.projects)
+        self.genes = [Gene(contributors, projects)]
+        for gene in self.genes:
+            gene.greedy_init()
+
+    def best_result(self):
+        return self.genes[0]
+
