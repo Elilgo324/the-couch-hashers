90cd66b5269fcfb12357fe0d6e01792546575cd4
ItayElizur <elizur.itay@gmail.con>
2022-08-29 14:41:36 +0300

    sorting funcs

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from typing import List
+from typing import List, Dict
 
 
 class Contributor:
-    def __init__(self, name: str, skillz_names: List[str], skillz_levels: List[int]):
+    def __init__(self, name: str, skillz: Dict[str, int]):
         self.name = name
-        self.skillz = skillz_names
-        self.skillz_levels = skillz_levels
+        self.skillz = skillz
 
     def __repr__(self):
         return f'Contributor(name={self.name}, skills={self.skillz})'
@@ -17,17 +16,27 @@ class Contributor:
 
 class Project:
     def __init__(self, project_name: str, project_length: int, project_score: int, project_deadline: int,
-                 roll_names: List[str], roll_levels: List[int]):
+                 rolls: Dict[str, int]):
         self.name = project_name
         self.length = project_length
         self.score = project_score
         self.deadline = project_deadline
-        self.rolls = roll_names
-        self.roll_levels = roll_levels
+        self.rolls = rolls
+        self.latest = self.deadline + self.score - 1
+        self.purple_score = self.score / self.length
 
     def __repr__(self):
         return f'Project(name={self.name}, length={self.length}, score={self.score}, deadline={self.deadline},' \
               f'rolls={self.rolls})'
+
+
+def sort_projects_by_purple_score(projects: List[Project]):
+    projects.sort(key=lambda p: p.purple_score)
+
+
+def find_most_fit_contributor(contributors: List[Contributor], project: Project):
+    for skill in project.rolls:
+        contributors.sort(key=lambda c: max(c.skillz[skill] - project.rolls[skill], 0))
 
 
 def parse():
@@ -37,24 +46,22 @@ def parse():
         contributor_intial = input().split(' ')
         name = contributor_intial[0]
         num_skillz = int(contributor_intial[1])
-        skillz_names, skillz_levels = [], []
+        skillz = {}
         for skill in range(num_skillz):
             skill = input().split(' ')
-            skillz_names.append(skill[0])
-            skillz_levels.append(int(skill[1]))
-        contributors.append(Contributor(name, skillz_names, skillz_levels))
+            skillz[skill[0]] = int(skill[1])
+        contributors.append(Contributor(name, skillz))
 
     projects = []
     for project in range(num_projects):
         project_intial = input().split(' ')
         project_name = project_intial[0]
         project_length, project_score, project_deadline, project_rolls = (int(project_intial[x]) for x in range(1, 5))
-        roll_names, roll_levels = [], []
+        rolls = {}
         for roll in range(project_rolls):
             roll = input().split(' ')
-            roll_names.append(roll[0])
-            roll_levels.append(int(roll[1]))
-        projects.append(Project(project_name, project_length, project_score, project_deadline, roll_names, roll_levels))
+            rolls[roll[0]] = int(roll[1])
+        projects.append(Project(project_name, project_length, project_score, project_deadline, rolls))
 
     return contributors, projects
 
