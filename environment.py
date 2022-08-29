12440da04118d34f6452e2ac2e53a72cf57a2059
ItayElizur <elizur.itay@gmail.con>
2022-08-29 15:49:18 +0300

    almost finished scoring

--- environment.py
+++ environment.py
@@ -9,6 +9,9 @@ class Contributor:
     def __repr__(self):
         return f'Contributor(name={self.name}, skills={self.skillz})'
 
+    def __hash__(self):
+        return hash(self.name)
+
     def improve_skill(self, skill_name: str):
         self.skillz[skill_name] += 1
 
@@ -27,6 +30,12 @@ class Project:
     def __repr__(self):
         return f'Project(name={self.name}, length={self.length}, score={self.score}, deadline={self.deadline},' \
               f'rolls={self.required_rolls})'
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def score_on_day(self, day_start: int):
+        return max(0, self.score - max(0, day_start + self.length - self.deadline))
 
 
     def get_greedy_contributors_assignment(self) -> List[Contributor]:
--- genetics.py
+++ genetics.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+import networkx as nx
+
 from environment import Contributor, Project
 
 
@@ -9,7 +11,33 @@ def calculate_max_days(projects: List[Project]):
 
 class Gene:
     def __init__(self, max_days, num_contributors):
-        self.gene_mat = [[None for _ in range(num_contributors)] for _ in range(max_days)]
+        self.start_day = {project: -1 for project in projects}
+        self.worked_on = {project: [] for project in projects}
+        self.working_on = {contributor: [] for contributor in contributors}
+
+    def _is_assignment_legit(self, project: Project, contributors: List[Contributor]):
+        graph = nx.Graph()
+        graph.add_nodes_from([contributor for contributor in contributors], bipartite=0)
+        graph.add_nodes_from([rr for rr in project.required_rolls], bipartite=1)
+        graph.add_edges_from([(contributor, rr) for rr in project.required_rolls for contributor in contributors
+                              if rr in contributor.skillz and contributor.skillz[rr] > project.required_rolls[rr]])
+        return nx.matching.maximal_matching(graph) == len(project.required_rolls)
+
+    def _is_date_legit(self):
+        for contributor in self.working_on:
+            last_day = -1
+            for project in self.working_on[contributor]:
+                if self.start_day[project] <= last_day:
+                    return False
+                last_day = self.start_day[project] + project.length
+        return True
+
+    def fitness(self):
+        score = 0
+        for project in self.start_day:
+            if self._is_assignment_legit(project, self.working_on[project]):
+                score += project.score_on_day(self.start_day[project])
+        return score
 
     def greedy_init(self, sorted_projects: Dict) -> None:
         self.gene_mat = [[]]
